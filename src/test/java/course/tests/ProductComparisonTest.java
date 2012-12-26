5a78f42bc944b078ca01c13297c831d9602d89a3
Denis Poznyak <tugu.by@gmail.com>
2012-12-26 23:59:42 +0300

    Prepared files for svn commit

--- src/test/java/course/tests/ProductComparisonTest.java
+++ src/test/java/course/tests/ProductComparisonTest.java
@@ -1,6 +1,5 @@
 package course.tests;
 
-import course.base.BaseFunctions;
 import course.forms.*;
 import webdriver.BaseTest;
 import webdriver.controller.UserType;
@@ -12,8 +11,6 @@ public class ProductComparisonTest extends BaseTest {
     public UsersController.User user = UsersController.getInstance().getUserByType(UserType.USER);
 
     public void runTest() {
-        BaseFunctions.windowMaximise(browser.getDriver());
-
         //1
         LogStep();
         HomeForm homeForm = new HomeForm();
